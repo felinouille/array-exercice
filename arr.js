71d353bf26b1bae5b3c48ea5cd44dd2843e33dca
felinouille <felicia.bcqt@gmail.com>
2026-01-29 21:59:12 +0100

    exercice

--- arr.js
+++ arr.js
@@ -44,3 +44,154 @@ function bubule(arr) {
 }
 
 console.log(bubule([2,6,8,4,2,7,9,3,5]));
+
+//////////////////////////////////////////////////////////////////////////////
+
+let temperatures = [18, 21, 15, 22, 19, 24, 20, 16, 23, 17];
+let sum = 0;
+let mid = 0;
+let hotTemperature = 0;
+let tempMax = temperatures[0];
+let tempMin = temperatures[0];
+
+for (let i = 0; i < temperatures.length; i++) {
+
+    sum += temperatures[i];
+
+    if (temperatures[i] >= 20) {
+        hotTemperature ++
+    }
+
+    if (temperatures[i] < tempMin) {
+        tempMin = temperatures[i];
+    }
+
+    if (temperatures[i] > tempMax) {
+        tempMax = temperatures[i];
+    }
+    
+}
+
+mid = sum / temperatures.length;
+
+console.log(sum);
+console.log(mid);
+console.log(hotTemperature);
+console.log(tempMax);
+console.log(tempMin);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+let classeA = [12, 15, 9, 18, 14];
+let classeB = [10, 17, 8, 20, 13, 16];
+let classeC = [14, 11, 19, 7, 15, 12];
+
+
+function calcule(arr) {
+
+    let notesSum = 0;
+    let notesMid = 0;
+    let goodNotes = 0;
+    let notesMin = arr[0];
+    let notesMax = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        notesSum += arr[i];
+
+        if (arr[i] >= 15) {
+        goodNotes ++
+        }
+
+        if (arr[i] < notesMin) {
+        notesMin = arr[i];
+        }
+
+        if (arr[i] > notesMax) {
+        notesMax = arr[i];
+        }
+    
+    }
+
+    notesMid = notesSum / arr.length;
+
+    console.log(notesSum);
+    console.log(notesMid);
+    console.log(goodNotes);
+    console.log(notesMax);
+    console.log(notesMin);
+}
+
+calcule(classeA);
+calcule(classeB);
+calcule(classeC);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let vente = [120, 150, 90, 200, 175, 130, 160, 95, 180, 110, 140, 210, 125, 170];
+let venteSum = 0;
+let venteMid = 0;
+let venteMin = vente[0];
+let venteMax = vente[0];
+let goodDay = 0;
+let badDay = 0;
+let normalDay = 0;
+
+for (let i =0; i < vente.length; i++) {
+
+    venteSum += vente[i];
+
+    if (vente[i] < venteMin) {
+        venteMin = vente[i];
+    }
+
+    if (vente[i] > venteMax) {
+        venteMax = vente[i];
+    }
+
+    if (vente[i] > 150) {
+        goodDay++
+    }
+
+    if (vente[i] < 100) {
+        badDay++
+    }
+
+    if (vente[i] >= 100 && vente[i] <= 150) {
+        normalDay++
+    }
+
+}
+
+venteMid = venteSum / vente.length;
+
+console.log(venteSum);
+console.log(venteMid);
+console.log(venteMin);
+console.log(venteMax);
+console.log(goodDay);
+console.log(normalDay);
+console.log(badDay);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let nombres = [3, 7, 2, 9, 12, 5, 8];
+let paire = [];
+let nombresFois2 = [];
+
+for (let i = 0; i < nombres.length; i++) {
+
+    if (nombres[i] % 2 == 0) {
+        paire.push(nombres[i]);
+    }
+
+    let newNumb = nombres[i] * 2;
+    nombresFois2.push(newNumb);
+}
+
+console.log(nombres);
+console.log(paire);
+console.log(nombresFois2);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
